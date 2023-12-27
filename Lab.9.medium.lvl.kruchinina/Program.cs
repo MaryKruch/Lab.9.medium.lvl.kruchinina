a573de0a283fb81955ebf20e68e9001122ea2557
1 <1@DESKTOP-ACMQRLH>
2023-12-27 22:58:56 +0200

    Добавьте файлы проекта.

---
+++ Lab.9.medium.lvl.kruchinina/Program.cs
@@ -0,0 +1,61 @@
+﻿// 2 var
+public class MyClass
+{
+    public int day;
+    public int year;
+    public int month;
+
+    public int Day
+    {
+        get { return day; }
+        set
+        {
+            if (value >= 0) day = value;
+            else day = 0;
+        }
+    }
+    public int Mounth
+    {
+        get { return month; }
+        set
+        {
+            if (value >= 0) month = value;
+            else month = 0;
+        }
+    }
+    public int Year
+    {
+        get { return year; }
+        set
+        {
+            if (value >= 0) year = value;
+            else year = 0;
+        }
+    }
+    public MyClass(int day, int month, int year)
+    {
+        Day = day;
+        Mounth = month;
+        Year = year;
+    }
+
+    public MyClass()
+    {
+        Day = 0;
+        Mounth = 0;
+        Year = 0;
+    }
+    public void AddYear()
+    {
+        Year += 1;
+    }
+
+    public void SubRwoDays()
+    {
+        DateTime date = new DateTime(Year, Mounth, Day);
+        date.Subtract(new TimeSpan(2));
+        Day = date.Day;
+        Mounth = date.Month;
+        Year = date.Year;
+    }
+}
